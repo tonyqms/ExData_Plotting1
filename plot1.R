4238914396daed6c6d7f57024e34213b0bb92f63
Tonyqms <tonyqms@gmail.com>
2015-03-03 22:24:48 -0600

    plot1

---
+++ plot1.R
@@ -0,0 +1,14 @@
+file <- "./R course/household_power_consumption.txt"
+data <- read.table(file, header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+sub <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
+rm(data)
+
+datetime <- paste(as.Date(sub$Date), sub$Time)
+sub$Datetime <- as.POSIXct(datetime)
+##plot1
+hist(sub$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+dev.copy(png, file="./GitHub/ExData_Plotting1/plot1.png", height=480, width=480)
+dev.off()
