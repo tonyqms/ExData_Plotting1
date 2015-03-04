fa5d8994477eeb048bf467b16558bab1ade0076d
Tonyqms <tonyqms@gmail.com>
2015-03-03 22:38:41 -0600

    plot4

---
+++ plot4.R
@@ -0,0 +1,28 @@
+file <- "./R course/household_power_consumption.txt"
+data <- read.table(file, header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+sub <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
+rm(data)
+
+datetime <- paste(as.Date(sub$Date), sub$Time)
+sub$Datetime <- as.POSIXct(datetime)
+##plot4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(sub, {
+        plot(Global_active_power~Datetime, type="l", 
+             ylab="Global Active Power (kilowatts)", xlab="")
+        plot(Voltage~Datetime, type="l", 
+             ylab="Voltage (volt)", xlab="")
+        plot(Sub_metering_1~Datetime, type="l", 
+             ylab="Global Active Power (kilowatts)", xlab="")
+        lines(Sub_metering_2~Datetime,col='Red')
+        lines(Sub_metering_3~Datetime,col='Blue')
+        legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+               legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        plot(Global_reactive_power~Datetime, type="l", 
+             ylab="Global Rective Power (kilowatts)",xlab="")
+})
+
+## Saving to file
+dev.copy(png, file="./GitHub/ExData_Plotting1/plot4.png", height=480, width=480)
+dev.off()
